29d15b3fac9669646908c0c7227ac1f1a157f422
Umar Irale <mailpersonal725@gmail.com>
2025-11-27 13:37:05 -0800

    chore: update dependencies and add Tailwind CSS integration
    - Added new dependencies: @radix-ui/react-slot, @tailwindcss/vite, @zama-fhe/relayer-sdk, class-variance-authority, clsx, ethers, framer-motion, lucide-react, motion, and tailwind-merge.
    - Updated @types/node to version 24.10.1.
    - Added tw-animate-css to dependencies.
    - Integrated Tailwind CSS into the project by importing it in App.css and configuring it in vite.config.ts.
    - Created a new components.json file for component configuration.
    - Added utility functions for class name merging in utils.ts.
    - Updated button component styles and variants.
    - Configured TypeScript paths for easier imports.

--- Insperion-frontend/vite.config.ts
+++ Insperion-frontend/vite.config.ts
@@ -1,13 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss()
+  ],
   resolve: {
     alias: {
       'motion/react': 'framer-motion',
-      '@zama-fhe/relayer-sdk': path.resolve(__dirname, 'node_modules/@zama-fhe/relayer-sdk/dist/browser')
+      '@zama-fhe/relayer-sdk': path.resolve(__dirname, 'node_modules/@zama-fhe/relayer-sdk/dist/browser'),
+            "@": path.resolve(__dirname, "./src"),
     }
   }
 });
